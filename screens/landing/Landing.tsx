de27c91dc046c4596b9c64739e04fa1053cf8df3
Chayakorn Vongbunsin <taecv.work@gmail.com>
2024-02-12 10:34:41 +0700

    add search bar

--- screens/landing/Landing.tsx
+++ screens/landing/Landing.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, TextInput } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import * as Location from "expo-location";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 import { RootStackParamList } from "../../types";
 
@@ -22,7 +23,11 @@ const Landing: React.FC<LandingProps> = () => {
   const [region, setRegion] = useState<RegionType>();
 
   useEffect(() => {
-    (async () => {
+    getCurrentLocation();
+  }, []);
+
+  const getCurrentLocation = async () => {
+    try {
       // Request permission to access the device's location
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== "granted") {
@@ -33,31 +38,56 @@ const Landing: React.FC<LandingProps> = () => {
       // Fetch the current location
       let location = await Location.getCurrentPositionAsync({});
       const { latitude, longitude } = location.coords;
-      console.log(latitude, longitude);
       setRegion({
         latitude,
         longitude,
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421,
       });
-    })();
-  }, []);
+    } catch (error) {
+      // Handle the error appropriately, such as displaying an error message to the user
+      console.error("Error getting current location:");
+    }
+  };
 
   return (
     <View style={{ flex: 1 }}>
       {region ? (
-        <MapView style={{ flex: 1 }} initialRegion={region}>
-          <Marker
-            draggable
-            coordinate={{
-              latitude: region.latitude,
-              longitude: region.longitude,
-            }}
-            title="Your Location"
-          />
-        </MapView>
+        <>
+          <MapView style={{ flex: 1 }} initialRegion={region}>
+            <Marker
+              draggable
+              coordinate={{
+                latitude: region.latitude,
+                longitude: region.longitude,
+              }}
+              title="Your Location"
+            />
+          </MapView>
+          <View style={{ position: "absolute", width: "100%" }}>
+            <SafeAreaView>
+              <TextInput
+                style={{
+                  borderRadius: 10,
+                  margin: 10,
+                  color: "#000",
+                  borderColor: "#666",
+                  backgroundColor: "#FFF",
+                  borderWidth: 1,
+                  height: 45,
+                  paddingHorizontal: 10,
+                  fontSize: 18,
+                }}
+                placeholder={"Search"}
+                placeholderTextColor={"#666"}
+              />
+            </SafeAreaView>
+          </View>
+        </>
       ) : (
-        <Text>Loading...</Text>
+        <SafeAreaView>
+          <Text>Loading...</Text>
+        </SafeAreaView>
       )}
     </View>
   );
