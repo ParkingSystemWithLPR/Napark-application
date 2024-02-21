90eb16523a8a0231d923e565ce9d44afacb836e8
kanin <88888222+maxkanin@users.noreply.github.com>
2024-02-21 17:04:34 +0700

    Add hyperLinkText component

---
+++ components/text/HyperlinkText.tsx
@@ -0,0 +1,55 @@
+import {
+  StyleSheet,
+  View,
+  Text,
+  Linking,
+  TouchableHighlight,
+} from "react-native";
+
+import { HyperlinkTextProps } from "../../types";
+import { useState } from "react";
+import Colors from "../../constants/color";
+
+const HyperLinkText: React.FC<HyperlinkTextProps> = ({
+  text,
+  containerStyle,
+  textStyle,
+  url,
+}) => {
+  const [isPressed, setIsPressed] = useState(false);
+
+  const handlePressIn = () => {
+    setIsPressed(true);
+  };
+
+  const handlePressOut = () => {
+    setIsPressed(false);
+  };
+  return (
+    <TouchableHighlight
+      style={[containerStyle]}
+      underlayColor="transparent"
+      onPressIn={handlePressIn}
+      onPressOut={handlePressOut}
+      onPress={() => {
+        Linking.openURL(url);
+      }}
+    >
+      <Text style={[styles.text, textStyle, isPressed && styles.highlighted]}>
+        {text}
+      </Text>
+    </TouchableHighlight>
+  );
+};
+
+export default HyperLinkText;
+
+const styles = StyleSheet.create({
+  text: {
+    fontFamily: "Poppins-Medium",
+    fontSize: 14,
+  },
+  highlighted: {
+    color: Colors.red[500],
+  },
+});
