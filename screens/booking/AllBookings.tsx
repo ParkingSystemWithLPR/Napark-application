ce017bf01d9b833e28f53ac9e3fd9dc57935bde1
Pattanan Piyachotinan <70986870+Petoxic@users.noreply.github.com>
2024-02-26 22:21:26 +0700

    remove mockData and use it from another file

--- screens/booking/AllBookings.tsx
+++ screens/booking/AllBookings.tsx
@@ -1,114 +1,8 @@
 import { View, StyleSheet, FlatList } from "react-native";
 
 import CompletedTimeSlot from "../../components/booking/CompletedTimeSlot";
-
-const mock = [
-  {
-    licensePlate: "กข 1334",
-    space: "space 4c",
-    date: "20/01/41",
-    time: "03:38 pm",
-    price: "200",
-  },
-  {
-    licensePlate: "กข 1334",
-    space: "space 4c",
-    date: "20/01/41",
-    time: "03:38 pm",
-    price: "200",
-  },
-  {
-    licensePlate: "กข 1334",
-    space: "space 4c",
-    date: "20/01/41",
-    time: "03:38 pm",
-    price: "200",
-  },
-  {
-    licensePlate: "กข 1334",
-    space: "space 4c",
-    date: "20/01/41",
-    time: "03:38 pm",
-    price: "200",
-  },
-  {
-    licensePlate: "กข 1334",
-    space: "space 4c",
-    date: "20/01/41",
-    time: "03:38 pm",
-    price: "200",
-  },
-  {
-    licensePlate: "กข 1334",
-    space: "space 4c",
-    date: "20/01/41",
-    time: "03:38 pm",
-    price: "200",
-  },
-  {
-    licensePlate: "กข 1334",
-    space: "space 4c",
-    date: "20/01/41",
-    time: "03:38 pm",
-    price: "200",
-  },
-  {
-    licensePlate: "กข 1334",
-    space: "space 4c",
-    date: "20/01/41",
-    time: "03:38 pm",
-    price: "200",
-  },
-  {
-    licensePlate: "กข 1334",
-    space: "space 4c",
-    date: "20/01/41",
-    time: "03:38 pm",
-    price: "200",
-  },
-  {
-    licensePlate: "กข 1334",
-    space: "space 4c",
-    date: "20/01/41",
-    time: "03:38 pm",
-    price: "200",
-  },
-  {
-    licensePlate: "กข 1334",
-    space: "space 4c",
-    date: "20/01/41",
-    time: "03:38 pm",
-    price: "200",
-  },
-  {
-    licensePlate: "กข 1334",
-    space: "space 4c",
-    date: "20/01/41",
-    time: "03:38 pm",
-    price: "200",
-  },
-  {
-    licensePlate: "กข 1334",
-    space: "space 4c",
-    date: "20/01/41",
-    time: "03:38 pm",
-    price: "200",
-  },
-  {
-    licensePlate: "กข 1334",
-    space: "space 4c",
-    date: "20/01/41",
-    time: "03:38 pm",
-    price: "200",
-  },
-  {
-    licensePlate: "กข 1334",
-    space: "space 4c",
-    date: "20/01/41",
-    time: "03:38 pm",
-    price: "200",
-  },
-];
+import { formatDateAndTime } from "../../utils/date";
+import { MOCKED_SESSIONS } from "../../utils/mockData";
 
 interface allBookingProps { }
 
@@ -116,16 +10,18 @@ const AllBookings: React.FC<allBookingProps> = () => {
   return (
     <View style={styles.container}>
       <FlatList
-        data={mock}
-        renderItem={({ item }) => (
-          <CompletedTimeSlot
+        data={MOCKED_SESSIONS}
+        renderItem={({ item }) => {
+          const dateAndTime = formatDateAndTime(new Date(item.dateAndTime)).split(" ");
+          return <CompletedTimeSlot
             licensePlate={item.licensePlate}
             space={item.space}
-            date={item.date}
-            time={item.time}
+            date={dateAndTime[0]}
+            time={dateAndTime[1]}
             price={item.price}
           />
-        )}
+        }
+        }
         showsVerticalScrollIndicator={false}
       />
     </View>
