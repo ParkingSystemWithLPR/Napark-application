ae2704e058cc9ec708ad6f8c4cc0438ce00dcc92
Pattanan Piyachotinan <70986870+Petoxic@users.noreply.github.com>
2024-02-26 14:30:58 +0700

    Edit MyBooking page

--- screens/booking/MyBooking.tsx
+++ screens/booking/MyBooking.tsx
@@ -1,19 +1,65 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { View, Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import React from "react";
+import { View, StyleSheet, Pressable, SafeAreaView } from "react-native";
 
+import ActiveTimeSlot from "../../components/booking/ActiveTimeSlot";
+import CompletedTimeSlot from "../../components/booking/CompletedTimeSlot";
+import SubHeaderText from "../../components/text/SubHeaderText";
+import Colors from "../../constants/color";
 import { RootParamList } from "../../types";
 
 export type MyBookingProps = NativeStackScreenProps<RootParamList, "MyBooking">;
 
-const MyBooking: React.FC<MyBookingProps> = () => {
+const MyBooking: React.FC<MyBookingProps> = ({ navigation }) => {
+  const onPress = () => {
+    navigation.navigate("AllBookings");
+  };
   return (
-    <View>
+    <View style={styles.container}>
       <SafeAreaView>
-        <Text>My booking</Text>
+        <SubHeaderText text="Active Session" textStyle={styles.activeSession} />
+        <ActiveTimeSlot />
+        <View style={styles.completedSessionsWrapper}>
+          <SubHeaderText
+            text="Completed Sessions"
+            textStyle={styles.completedSessionsText}
+          />
+          <Pressable onPress={onPress}>
+            <SubHeaderText text="View all" textStyle={styles.viewAllText} />
+          </Pressable>
+        </View>
+        <CompletedTimeSlot
+          licensePlate="หส 4142"
+          space="Spce f2"
+          date="13/03/41"
+          time="01:42 am"
+          price="141"
+        />
       </SafeAreaView>
     </View>
   );
 };
 
 export default MyBooking;
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 20,
+  },
+  activeSession: {
+    paddingTop: 20,
+    color: Colors.gray[900],
+  },
+  completedSessionsWrapper: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginVertical: 20,
+  },
+  completedSessionsText: {
+    color: Colors.gray[900],
+  },
+  viewAllText: {
+    color: Colors.red[500],
+    fontWeight: "bold",
+  },
+});
