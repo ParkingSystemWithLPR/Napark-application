e7a264ca26da7be5f2977545e42469c140596510
Pattanan Piyachotinan <70986870+Petoxic@users.noreply.github.com>
2024-02-26 14:55:48 +0700

    Add interface

--- components/booking/ActiveTimeSlot.tsx
+++ components/booking/ActiveTimeSlot.tsx
@@ -1,26 +1,33 @@
 import { View, StyleSheet } from "react-native";
+
 import Colors from "../../constants/color";
+import BodyText from "../text/BodyText";
 import HeaderText from "../text/HeaderText";
 import SubHeaderText from "../text/SubHeaderText";
-import BodyText from "../text/BodyText";
 
-const ActiveTimeSlot: React.FC<{}> = () => {
+interface activeTimeSlotProps {
+  licensePlate: string,
+  space: string,
+  timeRemaining: string
+}
+
+const ActiveTimeSlot: React.FC<activeTimeSlotProps> = ({ licensePlate, space, timeRemaining }) => {
   return (
     <View style={styles.wrapper}>
       <View style={styles.topContainer}>
         <HeaderText
-          text="กข 1234"
+          text={licensePlate}
           textStyle={{ color: Colors.black, fontWeight: "bold" }}
         />
         <BodyText text="Space 4c" textStyle={{ color: Colors.gray[900] }} />
       </View>
       <View style={styles.bottomContainer}>
         <SubHeaderText
-          text="Time Remaining"
+          text={space}
           textStyle={{ color: Colors.gray[900] }}
         />
         <SubHeaderText
-          text="01hr : 30min"
+          text={timeRemaining}
           textStyle={{ color: Colors.gray[900], fontWeight: "bold" }}
         />
       </View>
