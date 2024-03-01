0ac1728db3b2011458e5aecfe7aceb3207b93a03
Chayakorn Vongbunsin <taecv.work@gmail.com>
2024-03-01 21:36:15 +0700

    fix layout

--- screens/parkinglot/ParkingLotDetail.tsx
+++ screens/parkinglot/ParkingLotDetail.tsx
@@ -42,86 +42,88 @@ const ParkingLotDetail: React.FC<ParkingLotDetailProps> = ({ navigation }) => {
   return (
     <BodyContainer innerContainerStyle={styles.bodyContainer}>
       <ScrollView overScrollMode="never">
-        <HeaderText
-          text={parkingLot.name}
-          textStyle={{ color: Colors.gray[900], fontSize: 24 }}
-        />
-        <MapView
-          style={styles.map}
-          initialRegion={parkingLot.coord}
-          zoomEnabled
-        >
-          <Marker
-            coordinate={{
-              latitude: parkingLot.coord.latitude,
-              longitude: parkingLot.coord.longitude,
-            }}
-            title="Your Parking Space Location"
+        <View style={styles.bodyContainer}>
+          <HeaderText
+            text={parkingLot.name}
+            textStyle={{ color: Colors.gray[900], fontSize: 24 }}
           />
-        </MapView>
-        <View style={styles.buttonContainer}>
-          <IconButtonWithTitle
-            title={"Info"}
-            icon={"information-outline"}
-            onPress={() => {}}
-          />
-          <IconButtonWithTitle
-            title={"Plan"}
-            icon={"floor-plan"}
-            onPress={() => {}}
-          />
-          <IconButtonWithTitle
-            title={"Pricing"}
-            icon={"bank"}
-            onPress={() => {}}
-          />
-          <IconButtonWithTitle
-            title={"Role"}
-            icon={"head-cog-outline"}
-            onPress={() => {
-              navigation.navigate("OtherStack", { screen: "RoleList" });
-            }}
-          />
+          <MapView
+            style={styles.map}
+            initialRegion={parkingLot.coord}
+            zoomEnabled
+          >
+            <Marker
+              coordinate={{
+                latitude: parkingLot.coord.latitude,
+                longitude: parkingLot.coord.longitude,
+              }}
+              title="Your Parking Space Location"
+            />
+          </MapView>
+          <View style={styles.buttonContainer}>
+            <IconButtonWithTitle
+              title={"Info"}
+              icon={"information-outline"}
+              onPress={() => {}}
+            />
+            <IconButtonWithTitle
+              title={"Plan"}
+              icon={"floor-plan"}
+              onPress={() => {}}
+            />
+            <IconButtonWithTitle
+              title={"Pricing"}
+              icon={"bank"}
+              onPress={() => {}}
+            />
+            <IconButtonWithTitle
+              title={"Role"}
+              icon={"head-cog-outline"}
+              onPress={() => {
+                navigation.navigate("OtherStack", { screen: "RoleList" });
+              }}
+            />
+          </View>
+          <SectionAppForm title={"Photos"} icon={"camera"}>
+            <ImageContainer images={["image1", "image2"]} />
+          </SectionAppForm>
+          <SectionAppForm title={"Location"} icon={"google-maps"}>
+            <BodyText
+              textStyle={styles.text}
+              text={formatAddress({
+                address: parkingLot.address,
+                sub_distict: parkingLot.sub_distict,
+                distict: parkingLot.distict,
+                province: parkingLot.province,
+                zip_code: parkingLot.zip_code,
+              })}
+            />
+          </SectionAppForm>
+          <SectionAppForm title={"Traffic"} icon={"car"}>
+            <View style={styles.textWrapper}>
+              <BodyText text={"1st floor"} textStyle={styles.text} />
+              <BodyText text={"20/20"} />
+            </View>
+            <View style={styles.textWrapper}>
+              <BodyText text={"2st floor"} textStyle={styles.text} />
+              <BodyText text={"19/20"} />
+            </View>
+            <View style={styles.textWrapper}>
+              <BodyText text={"3st floor"} textStyle={styles.text} />
+              <BodyText text={"20/20"} />
+            </View>
+            <View style={styles.textWrapper}>
+              <BodyText text={"4st floor"} textStyle={styles.text} />
+              <BodyText text={"11/12"} />
+            </View>
+          </SectionAppForm>
+          <SectionAppForm title={"Business hours"} icon={"clock"}>
+            <View style={styles.textWrapper}>
+              <BodyText text={"Monday - Friday"} textStyle={styles.text} />
+              <BodyText text={"06:30 - 23.59"} />
+            </View>
+          </SectionAppForm>
         </View>
-        <SectionAppForm title={"Photos"} icon={"camera"}>
-          <ImageContainer images={["image1", "image2"]} />
-        </SectionAppForm>
-        <SectionAppForm title={"Location"} icon={"google-maps"}>
-          <BodyText
-            textStyle={styles.text}
-            text={formatAddress({
-              address: parkingLot.address,
-              sub_distict: parkingLot.sub_distict,
-              distict: parkingLot.distict,
-              province: parkingLot.province,
-              zip_code: parkingLot.zip_code,
-            })}
-          />
-        </SectionAppForm>
-        <SectionAppForm title={"Traffic"} icon={"car"}>
-          <View style={styles.textWrapper}>
-            <BodyText text={"1st floor"} textStyle={styles.text} />
-            <BodyText text={"20/20"} />
-          </View>
-          <View style={styles.textWrapper}>
-            <BodyText text={"2st floor"} textStyle={styles.text} />
-            <BodyText text={"19/20"} />
-          </View>
-          <View style={styles.textWrapper}>
-            <BodyText text={"3st floor"} textStyle={styles.text} />
-            <BodyText text={"20/20"} />
-          </View>
-          <View style={styles.textWrapper}>
-            <BodyText text={"4st floor"} textStyle={styles.text} />
-            <BodyText text={"11/12"} />
-          </View>
-        </SectionAppForm>
-        <SectionAppForm title={"Business hours"} icon={"clock"}>
-          <View style={styles.textWrapper}>
-            <BodyText text={"Monday - Friday"} textStyle={styles.text} />
-            <BodyText text={"06:30 - 23.59"} />
-          </View>
-        </SectionAppForm>
       </ScrollView>
     </BodyContainer>
   );
