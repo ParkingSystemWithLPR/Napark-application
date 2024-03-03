724fe10d980743b647a03cb3b4fd2fbc7058bc9d
ing <88963106+ingthanhunt@users.noreply.github.com>
2024-03-04 00:36:52 +0700

    [NP-75] feat: add role & config parking space page

--- screens/role/RoleList.tsx
+++ screens/role/RoleList.tsx
@@ -2,10 +2,12 @@ import { CompositeScreenProps } from "@react-navigation/native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { StyleSheet, View } from "react-native";
 
+import PrimaryButton from "@/components/button/PrimaryButton";
 import RoleCard from "@/components/card/RoleCard";
 import SubHeaderText from "@/components/text/SubHeaderText";
 import BodyContainer from "@/components/ui/BodyContainer";
 import Colors from "@/constants/color";
+import { ActionMode } from "@/enum/ActionMode";
 import { OtherStackParamList, AuthenticatedStackParamList } from "@/types";
 
 export type RoleListProps = CompositeScreenProps<
@@ -13,19 +15,38 @@ export type RoleListProps = CompositeScreenProps<
   NativeStackScreenProps<AuthenticatedStackParamList>
 >;
 
-const RoleList: React.FC<RoleListProps> = () => {
+const RoleList: React.FC<RoleListProps> = ({ navigation }) => {
   return (
-    <BodyContainer>
-      <SubHeaderText text="Everybody" />
-      <View style={styles.roleCardContainer}>
-        <RoleCard roleName="Everybody" member="All" onPress={() => {}} />
+    <BodyContainer innerContainerStyle={styles.container}>
+      <View>
+        <SubHeaderText text="Everybody" />
+        <View style={styles.roleCardContainer}>
+          <RoleCard
+            roleName="Everybody"
+            member="All"
+            onPress={() =>
+              navigation.navigate("ConfigRole", {
+                mode: ActionMode.EDIT,
+                roleId: "all",
+              })
+            }
+          />
+        </View>
+        <View style={styles.divider}></View>
+        <SubHeaderText text="Other Roles" />
+        <View style={styles.roleCardContainer}>
+          <RoleCard roleName="VVIP" member="2" onPress={() => {}} />
+          <RoleCard roleName="C-level" member="10" onPress={() => {}} />
+        </View>
       </View>
-      <View style={styles.divider}></View>
-      <SubHeaderText text="Other Roles" />
-      <View style={styles.roleCardContainer}>
-        <RoleCard roleName="VVIP" member="2" onPress={() => {}} />
-        <RoleCard roleName="C-level" member="10" onPress={() => {}} />
-      </View>
+      <PrimaryButton
+        title="+ Add new role"
+        onPress={() =>
+          navigation.navigate("ConfigRole", {
+            mode: ActionMode.CREATE,
+          })
+        }
+      />
     </BodyContainer>
   );
 };
@@ -33,6 +54,10 @@ const RoleList: React.FC<RoleListProps> = () => {
 export default RoleList;
 
 const styles = StyleSheet.create({
+  container: {
+    justifyContent: "space-between",
+    paddingBottom: 50,
+  },
   divider: {
     borderWidth: 0.5,
     borderColor: Colors.red[200],
--- types/parking-lot/ParkingLot.ts
+++ types/parking-lot/ParkingLot.ts
@@ -3,11 +3,7 @@ import { Region } from "react-native-maps";
 export type ParkingLot = {
   _id: string,
   name: string,
-  address: string,
-  sub_distict: string,
-  distict: string;
-  province: string;
-  zip_code: string;
+  address: Address;
   coord: Region,
   businessHours?: string,
   availability?: number,
