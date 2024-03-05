6cc691e1e0f4dbb56270c58906d255e188ce1303
Pattanan Piyachotinan <70986870+Petoxic@users.noreply.github.com>
2024-03-05 23:27:34 +0700

    Fix Code format

--- navigation/OtherStack.tsx
+++ navigation/OtherStack.tsx
@@ -135,30 +135,6 @@ const OtherStack = () => {
           headerLeft: backToOtherPage,
         }}
       />
-      <Stack.Screen
-        name="AccountAndCard"
-        component={AccountAndCard}
-        options={{
-          title: "Account and Card",
-          headerLeft: backToOtherPage,
-        }}
-      />
-      <Stack.Screen
-        name="ScanQR"
-        component={ScanQR}
-        options={{
-          title: "Scan QR",
-          headerLeft: backToOtherPage,
-        }}
-      />
-      <Stack.Screen
-        name="PayTheBill"
-        component={PayTheBill}
-        options={{
-          title: "Pay the bill",
-          headerLeft: backToOtherPage,
-        }}
-      />
     </Stack.Navigator>
   );
 };
