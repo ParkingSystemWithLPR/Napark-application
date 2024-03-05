9b03ae85036494f577a41dee4b6649a18849ef07
Pattanan Piyachotinan <70986870+Petoxic@users.noreply.github.com>
2024-03-05 22:16:16 +0700

    Add template pages

--- navigation/OtherStack.tsx
+++ navigation/OtherStack.tsx
@@ -132,6 +132,30 @@ const OtherStack = () => {
           headerLeft: backToOtherPage,
         }}
       />
+      <Stack.Screen
+        name="AccountAndCard"
+        component={AccountAndCard}
+        options={{
+          title: "Account and Card",
+          headerLeft: backToOtherPage,
+        }}
+      />
+      <Stack.Screen
+        name="ScanQR"
+        component={ScanQR}
+        options={{
+          title: "Scan QR",
+          headerLeft: backToOtherPage,
+        }}
+      />
+      <Stack.Screen
+        name="PayTheBill"
+        component={PayTheBill}
+        options={{
+          title: "Pay the bill",
+          headerLeft: backToOtherPage,
+        }}
+      />
     </Stack.Navigator>
   );
 };
