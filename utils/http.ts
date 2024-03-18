908fcbe7a870d292c5b7ba157aee2d8b9f6a31fa
Chayakorn Vongbunsin <taecv.work@gmail.com>
2024-03-18 09:29:46 +0700

    Update http.ts

--- utils/http.ts
+++ utils/http.ts
@@ -56,7 +56,6 @@ const apiRequest = async <T>(
       // Retry the request with the new access token
       return apiRequest<T>(url, method, access_token, authenticate, body);
     }
-    console.log(JSON.stringify(error));
     throw error;
   }
 };
