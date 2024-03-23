a9e2cc1bad802069e9a8bd29852693cf06046e42
kanin <88888222+maxkanin@users.noreply.github.com>
2024-03-23 11:33:13 +0700

    change licenseplate card

--- screens/payment/PayTheBill.tsx
+++ screens/payment/PayTheBill.tsx
@@ -1,9 +1,10 @@
 import { CompositeScreenProps } from "@react-navigation/native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { useCallback } from "react";
-import { FlatList, TouchableOpacity, View, StyleSheet } from "react-native";
-import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import { FlatList, View, StyleSheet, Pressable, Platform } from "react-native";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
+import HeaderText from "@/components/text/HeaderText";
 import SubHeaderText from "@/components/text/SubHeaderText";
 import BodyContainer from "@/components/ui/BodyContainer";
 import Colors from "@/constants/color";
@@ -26,12 +27,28 @@ const PayTheBill: React.FC<PayTheBillProps> = ({ navigation }) => {
   const renderLicensePlateList = useCallback(
     (item: mockedPaymentLicensePlateProps) => {
       return (
-        <TouchableOpacity onPress={pressLicensePlateHandler}>
-          <View style={styles.licensePlate}>
-            <SubHeaderText text={item.licensePlate} />
-            <MaterialIcons name="chevron-right" size={20}></MaterialIcons>
-          </View>
-        </TouchableOpacity>
+        <View style={styles.container}>
+          <Pressable
+            android_ripple={{ color: Colors.gray[600] }}
+            style={({ pressed }) => [pressed && styles.pressed]}
+            onPress={() => pressLicensePlateHandler()}
+          >
+            <View style={styles.innerContainer}>
+              <View style={styles.infoContainer}>
+                <HeaderText
+                  text={item.licensePlate}
+                  textStyle={styles.licenseText}
+                />
+                <SubHeaderText text={item.province} />
+              </View>
+              <MaterialCommunityIcons
+                name="chevron-right"
+                size={20}
+                color={Colors.gray[800]}
+              />
+            </View>
+          </Pressable>
+        </View>
       );
     },
     []
@@ -53,16 +70,39 @@ const PayTheBill: React.FC<PayTheBillProps> = ({ navigation }) => {
 export default PayTheBill;
 
 const styles = StyleSheet.create({
-  licensePlate: {
+  container: {
+    backgroundColor: Colors.white,
+    shadowColor: Colors.black,
+    borderRadius: 8,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+    elevation: Platform.OS === "android" ? 4 : 2,
+    marginVertical: 5,
+    marginHorizontal: 10,
+  },
+  innerContainer: {
+    width: "100%",
     flexDirection: "row",
     justifyContent: "space-between",
-    paddingHorizontal: 10,
-    paddingVertical: 15,
-    backgroundColor: Colors.white,
-    borderWidth: 1,
-    margin: 10,
-    borderRadius: 10,
     alignItems: "center",
-    borderColor: Colors.gray[800],
+    padding: 10,
+  },
+  infoContainer: {},
+  licenseText: {
+    color: Colors.black,
+  },
+  pressed: {
+    opacity: 0.5,
+  },
+  deleteButton: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.red[400],
+    height: "100%",
+    width: 100,
   },
 });
