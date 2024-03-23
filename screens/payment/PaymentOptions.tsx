dc715d0dbe5da43c028f03f82dc90c166a262904
Pattanan Piyachotinan <70986870+Petoxic@users.noreply.github.com>
2024-03-23 12:06:12 +0700

    Remove unused import

--- screens/payment/PaymentOptions.tsx
+++ screens/payment/PaymentOptions.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet } from "react-native";
 
 import ChangeScreenTab from "@/components/button/ChangeScreenTab";
 import SubHeaderText from "@/components/text/SubHeaderText";
