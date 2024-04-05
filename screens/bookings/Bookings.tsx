d9091d95f5a1601ebd6ecc3c9405a81d35f4c02a
Chayakorn Vongbunsin <taecv.work@gmail.com>
2024-04-05 23:43:45 +0700

    move active and completed booking to be in bookings page

--- screens/bookings/Bookings.tsx
+++ screens/bookings/Bookings.tsx
@@ -1,8 +1,8 @@
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import { CompositeScreenProps } from "@react-navigation/native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { StyleSheet, View } from "react-native";
 
-import SubHeaderText from "@/components/text/SubHeaderText";
 import BodyContainer from "@/components/ui/BodyContainer";
 import Colors from "@/constants/color";
 import {
@@ -13,26 +13,59 @@ import SecondaryButton from "@/components/button/SecondaryButton";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import BodyText from "@/components/text/BodyText";
 import HeaderText from "@/components/text/HeaderText";
+import SessionsList from "@/components/booking/SessionsList";
+import { BookingType } from "@/enum/BookingType";
+import { useCallback } from "react";
 
 export type BookingsProps = CompositeScreenProps<
   NativeStackScreenProps<MainPageBottomTabParamList, "Bookings">,
   NativeStackScreenProps<AuthenticatedStackParamList>
 >;
 
+const Tab = createMaterialTopTabNavigator();
+
 const mockBalance = 555.99;
 
+const capitalizeFirstLetter = (word: string) => {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+};
+
+const capitalizedActiveBookingType = capitalizeFirstLetter(BookingType.ACTIVE);
+const capitalizedCompletedBookingType = capitalizeFirstLetter(
+  BookingType.COMPLETED
+);
+
 const Bookings: React.FC<BookingsProps> = ({ navigation }) => {
+  const renderActiveBookings = useCallback(() => {
+    return (
+      <View style={styles.tabContent}>
+        <SessionsList type={BookingType.ACTIVE} />
+      </View>
+    );
+  }, []);
+
+  const renderCompletedBookings = useCallback(() => {
+    return (
+      <View style={styles.tabContent}>
+        <SessionsList type={BookingType.COMPLETED} />
+      </View>
+    );
+  }, []);
+
   return (
-    <BodyContainer>
+    <BodyContainer innerContainerStyle={styles.container}>
       <View style={styles.wallet}>
         <View style={styles.walletRow}>
           <View style={styles.myWallet}>
-            <Ionicons name="wallet-outline" size={20} color={Colors.white}/>
-            <BodyText text={"My Wallet"} textStyle={{color: Colors.white}} />
+            <Ionicons name="wallet-outline" size={20} color={Colors.white} />
+            <BodyText text={"My Wallet"} textStyle={{ color: Colors.white }} />
           </View>
         </View>
         <View style={styles.walletRow}>
-          <HeaderText text={`฿ ${mockBalance}`} textStyle={{color: Colors.black, fontSize: 18 }}/>
+          <HeaderText
+            text={`฿ ${mockBalance}`}
+            textStyle={{ color: Colors.black, fontSize: 18 }}
+          />
           <SecondaryButton
             onPress={() => {
               navigation.navigate("BookingsStack", {
@@ -46,6 +79,16 @@ const Bookings: React.FC<BookingsProps> = ({ navigation }) => {
           />
         </View>
       </View>
+      <Tab.Navigator>
+        <Tab.Screen
+          name={capitalizedActiveBookingType}
+          component={renderActiveBookings}
+        />
+        <Tab.Screen
+          name={capitalizedCompletedBookingType}
+          component={renderCompletedBookings}
+        />
+      </Tab.Navigator>
     </BodyContainer>
   );
 };
@@ -53,11 +96,16 @@ const Bookings: React.FC<BookingsProps> = ({ navigation }) => {
 export default Bookings;
 
 const styles = StyleSheet.create({
+  container: {
+    gap: 10,
+    paddingHorizontal:0,
+  },
   wallet: {
     alignItems: "center",
     justifyContent: "center",
     gap: 10,
     paddingVertical: 20,
+    marginHorizontal: 20,
     shadowColor: Colors.black,
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.2,
@@ -73,13 +121,13 @@ const styles = StyleSheet.create({
     width: "100%",
     paddingHorizontal: 20,
   },
-  myWallet:{
-    flexDirection:"row",
+  myWallet: {
+    flexDirection: "row",
     alignItems: "center",
     padding: 5,
     gap: 5,
     borderRadius: 8,
-    backgroundColor: Colors.red[600]
+    backgroundColor: Colors.red[600],
   },
   topUpButton: {
     backgroundColor: Colors.white,
@@ -87,7 +135,11 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: Colors.red[600],
   },
-  topUpButtonText:{
+  topUpButtonText: {
     color: Colors.red[600],
+  },
+  tabContent: {
+    flex: 1,
+    backgroundColor: Colors.gray[50],
   },
 });
