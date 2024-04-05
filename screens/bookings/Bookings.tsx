5628ecba57014bf24b1daeb5d954a4a55584e6f5
Chayakorn Vongbunsin <taecv.work@gmail.com>
2024-04-05 23:04:19 +0700

    Make a wallet

--- screens/bookings/Bookings.tsx
+++ screens/bookings/Bookings.tsx
@@ -2,8 +2,6 @@ import { CompositeScreenProps } from "@react-navigation/native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { StyleSheet, View } from "react-native";
 
-import IconButtonWithTitle from "@/components/button/IconButtonWithTitle";
-import HeaderText from "@/components/text/HeaderText";
 import SubHeaderText from "@/components/text/SubHeaderText";
 import BodyContainer from "@/components/ui/BodyContainer";
 import Colors from "@/constants/color";
@@ -11,6 +9,10 @@ import {
   MainPageBottomTabParamList,
   AuthenticatedStackParamList,
 } from "@/types";
+import SecondaryButton from "@/components/button/SecondaryButton";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import BodyText from "@/components/text/BodyText";
+import HeaderText from "@/components/text/HeaderText";
 
 export type BookingsProps = CompositeScreenProps<
   NativeStackScreenProps<MainPageBottomTabParamList, "Bookings">,
@@ -22,31 +24,27 @@ const mockBalance = 555.99;
 const Bookings: React.FC<BookingsProps> = ({ navigation }) => {
   return (
     <BodyContainer>
-      <View style={styles.card}>
-        <SubHeaderText
-          text="Credit Balance"
-          textStyle={styles.creditBalanceText}
-        />
-        <HeaderText
-          text={`฿ ${mockBalance}`}
-          textStyle={{ color: Colors.black }}
-        />
-      </View>
-      <View style={styles.buttonWrapper}>
-        <IconButtonWithTitle
-          title="Top up"
-          onPress={() => {
-            navigation.navigate("BookingsStack", {
-              screen: "TopUp",
-              params: { balance: mockBalance },
-            });
-          }}
-          containerStyle={styles.buttonContainer}
-          textStyle={styles.textContainer}
-          icon="cash"
-          iconColor={Colors.green[700]}
-          iconSize={40}
-        />
+      <View style={styles.wallet}>
+        <View style={styles.walletRow}>
+          <View style={styles.myWallet}>
+            <Ionicons name="wallet-outline" size={20} color={Colors.white}/>
+            <BodyText text={"My Wallet"} textStyle={{color: Colors.white}} />
+          </View>
+        </View>
+        <View style={styles.walletRow}>
+          <HeaderText text={`฿ ${mockBalance}`} textStyle={{color: Colors.black, fontSize: 18 }}/>
+          <SecondaryButton
+            onPress={() => {
+              navigation.navigate("BookingsStack", {
+                screen: "TopUp",
+                params: { balance: mockBalance },
+              });
+            }}
+            buttonStyle={styles.topUpButton}
+            textStyle={styles.topUpButtonText}
+            title="+ Top Up"
+          />
+        </View>
       </View>
     </BodyContainer>
   );
@@ -55,35 +53,41 @@ const Bookings: React.FC<BookingsProps> = ({ navigation }) => {
 export default Bookings;
 
 const styles = StyleSheet.create({
-  buttonWrapper: {
-    flexDirection: "row",
-    justifyContent: "space-evenly",
-    paddingTop: 30,
-  },
-  buttonContainer: {
-    paddingHorizontal: 8,
-    width: 110,
-    height: 110,
-  },
-  textContainer: {
-    color: Colors.gray[800],
-    textAlign: "center",
-    fontSize: 16,
-  },
-  card: {
+  wallet: {
     alignItems: "center",
+    justifyContent: "center",
     gap: 10,
-    paddingVertical: 50,
+    paddingVertical: 20,
     shadowColor: Colors.black,
-    shadowOffset: { width: 2, height: 4 },
+    shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.2,
     shadowRadius: 3,
     elevation: 4,
     borderRadius: 16,
     backgroundColor: Colors.white,
   },
-  creditBalanceText: {
-    fontSize: 20,
-    color: Colors.red[400],
+  walletRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    paddingHorizontal: 20,
+  },
+  myWallet:{
+    flexDirection:"row",
+    alignItems: "center",
+    padding: 5,
+    gap: 5,
+    borderRadius: 8,
+    backgroundColor: Colors.red[600]
+  },
+  topUpButton: {
+    backgroundColor: Colors.white,
+    borderRadius: 16,
+    borderWidth: 1,
+    borderColor: Colors.red[600],
+  },
+  topUpButtonText:{
+    color: Colors.red[600],
   },
 });
