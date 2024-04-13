7c7df13043be7a1778ed1e110aa7b0fbd65b4dd5
ing <88963106+ingthanhunt@users.noreply.github.com>
2024-04-13 20:47:47 +0700

    [NP-196] feat: change parking detail to use data from db (#51)
    ## **Ticket**
    
    **JIRA ID:**
    [NP-196](https://parking-system.atlassian.net/jira/software/projects/NP/boards/1/backlog?assignee=6298566eb407cc0069004048&selectedIssue=NP-196)
    
    ## Changes proposed in this pull request
    - Update parking space detail page to use data from db
    
    please fill all the required fields from below Checklist otherwise
    reviewer **will not review** this PR
    ## Checklist
    
    - [x] Screenshot evidence for both IOS and Android
    - [x] Total change is less than 500 lines of code
    
    ## **Screenshot Evidence**
    ![Screenshot 2567-04-13 at 20 38
    26](https://github.com/ParkingSystemWithLPR/Napark-application/assets/88963106/3a33b1aa-5965-466e-8421-2650f7a598db)
    
    [NP-196]:
    https://parking-system.atlassian.net/browse/NP-196?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- components/parking/ParkingBasicInfo.tsx
+++ components/parking/ParkingBasicInfo.tsx
@@ -1,17 +1,91 @@
 import { StyleSheet, View } from "react-native";
+import { useEffect, useState } from "react";
 
 import DetailText from "../text/DetailText";
 import SectionAppForm from "../ui/SectionAppForm";
 
 import Colors from "@/constants/color";
-import { ParkingLot } from "@/types/parking-lot";
+import { DayInAWeek } from "@/enum/DayInAWeek";
+import { Availability, BusinessHour, ParkingLot } from "@/types/parking-lot";
 import { formatAddress } from "@/utils/address";
+import {
+  formatDayRange,
+  formatDisplayTime,
+  isEqualBusinessHour,
+} from "@/utils/date";
 
 export type ParkingBasicInfoProps = {
   parkingLot: ParkingLot;
 };
 
+type BusinessHourSet = {
+  days: string[];
+  businessHour: BusinessHour;
+};
+
+type BusinessDaySet = {
+  days: string;
+  businessHour: string;
+};
+
 const ParkingBasicInfo: React.FC<ParkingBasicInfoProps> = ({ parkingLot }) => {
+  const { business_days, slots } = parkingLot;
+  const [businessDaySet, setBusinessDaySet] = useState<BusinessDaySet[]>();
+  const [availability, setAvailability] = useState<{
+    [floor: number]: { [zone: string]: Availability };
+  }>();
+
+  useEffect(() => {
+    const businessHoursSet: BusinessHourSet[] = [];
+    const newBusinessDaySet: BusinessDaySet[] = [];
+    business_days.forEach((businessDay) => {
+      const { weekday, close_time, open_time } = businessDay;
+      if (
+        !businessHoursSet.some((e) =>
+          isEqualBusinessHour(e.businessHour, { close_time, open_time })
+        )
+      ) {
+        businessHoursSet.push({
+          days: [],
+          businessHour: { close_time, open_time },
+        });
+      }
+      const index = businessHoursSet.findIndex((e) =>
+        isEqualBusinessHour(e.businessHour, { close_time, open_time })
+      );
+      businessHoursSet[index].days.push(weekday);
+    });
+    businessHoursSet.forEach((businessHourSet) => {
+      const { days, businessHour } = businessHourSet;
+      newBusinessDaySet.push({
+        days: formatDayRange(days as DayInAWeek[]),
+        businessHour: `${formatDisplayTime(
+          businessHour.open_time
+        )} - ${formatDisplayTime(businessHour.close_time)}`,
+      });
+    });
+    setBusinessDaySet(newBusinessDaySet);
+  }, [business_days]);
+
+  useEffect(() => {
+    const newAvailability: {
+      [floor: number]: { [zone: string]: Availability };
+    } = {};
+    slots.forEach((slot) => {
+      const { floor, zone, is_vacant } = slot;
+      const vacant = is_vacant ? 1 : 0;
+      if (newAvailability[floor] && newAvailability[floor][zone]) {
+        newAvailability[floor][zone].vacant += vacant;
+        newAvailability[floor][zone].capacity += 1;
+      } else if (newAvailability[floor]) {
+        newAvailability[floor][zone] = { vacant, capacity: 1 };
+      } else {
+        newAvailability[floor] = { [`${zone}`]: { vacant, capacity: 1 } };
+      }
+    });
+    setAvailability(newAvailability);
+  }, [slots]);
+
   return (
     <>
       <SectionAppForm title={"Location"} icon={"google-maps"}>
@@ -21,28 +95,28 @@ const ParkingBasicInfo: React.FC<ParkingBasicInfoProps> = ({ parkingLot }) => {
         />
       </SectionAppForm>
       <SectionAppForm title={"Traffic"} icon={"car"}>
-        <View style={styles.textWrapper}>
-          <DetailText text={"1st floor"} textStyle={styles.text} />
-          <DetailText text={"20/20"} />
-        </View>
-        <View style={styles.textWrapper}>
-          <DetailText text={"2st floor"} textStyle={styles.text} />
-          <DetailText text={"19/20"} />
-        </View>
-        <View style={styles.textWrapper}>
-          <DetailText text={"3st floor"} textStyle={styles.text} />
-          <DetailText text={"20/20"} />
-        </View>
-        <View style={styles.textWrapper}>
-          <DetailText text={"4st floor"} textStyle={styles.text} />
-          <DetailText text={"11/12"} />
-        </View>
+        {availability &&
+          Object.entries(availability).map(([key, value]) => (
+            <View key={key}>
+              <DetailText text={`Floor ${key}`} />
+              {Object.entries(value).map(([xkey, item]) => (
+                <View key={`${key}_${xkey}`} style={styles.subTextWrapper}>
+                  <DetailText text={`zone ${xkey}`} textStyle={styles.text} />
+                  <DetailText
+                    text={`${item.capacity - item.vacant} / ${item.capacity}`}
+                  />
+                </View>
+              ))}
+            </View>
+          ))}
       </SectionAppForm>
       <SectionAppForm title={"Business hours"} icon={"clock"}>
-        <View style={styles.textWrapper}>
-          <DetailText text={"Monday - Friday"} textStyle={styles.text} />
-          <DetailText text={"06:30 - 23.59"} />
-        </View>
+        {businessDaySet?.map((business_day, index) => (
+          <View key={index} style={styles.textWrapper}>
+            <DetailText text={business_day.days} textStyle={styles.text} />
+            <DetailText text={business_day.businessHour} />
+          </View>
+        ))}
       </SectionAppForm>
     </>
   );
@@ -54,6 +128,12 @@ const styles = StyleSheet.create({
   textWrapper: {
     flexDirection: "row",
     justifyContent: "space-between",
+    marginRight: 10,
+  },
+  subTextWrapper: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginHorizontal: 10,
   },
   text: {
     color: Colors.gray[900],
