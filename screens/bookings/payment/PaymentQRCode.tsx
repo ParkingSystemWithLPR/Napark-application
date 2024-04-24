fd9138e0aa9a44dfd991398bebe40647e7b789b8
Pattanan Piyachotinan <70986870+Petoxic@users.noreply.github.com>
2024-04-24 23:22:46 +0700

    Add qrcode retrieval api

--- screens/bookings/payment/PaymentQRCode.tsx
+++ screens/bookings/payment/PaymentQRCode.tsx
@@ -1,13 +1,97 @@
-import { Text, View } from "react-native";
+import { CompositeScreenProps } from "@react-navigation/native";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { useCallback, useEffect, useState } from "react";
+import { StyleSheet, View, Image } from "react-native";
 
-interface PaymentQRCodeProps {}
+import SecondaryButton from "@/components/button/SecondaryButton";
+import BodyText from "@/components/text/BodyText";
+import BodyContainer from "@/components/ui/BodyContainer";
+import Colors from "@/constants/color";
+import { MOCKED_QR_CODE_URL } from "@/mock/mockData";
+import { useGetTopUpQRCode } from "@/store/api/payment/useGetTopUpQRCode";
+import { useAuth } from "@/store/context/auth";
+import { BookingsStackParamList, AuthenticatedStackParamList } from "@/types";
+import { QRCode } from "@/types/payment";
+import { formatDateAndTime } from "@/utils/date";
 
-const PaymentQRCode: React.FC<PaymentQRCodeProps> = () => {
+export type PaymentQRCodeProps = CompositeScreenProps<
+  NativeStackScreenProps<BookingsStackParamList, "PaymentQRCode">,
+  NativeStackScreenProps<AuthenticatedStackParamList>
+>;
+
+const IMAGE_SIZE = 250;
+
+const PaymentQRCode: React.FC<PaymentQRCodeProps> = ({ route }) => {
+  const amount = route.params.amount;
+  const { accessToken, authenticate } = useAuth();
+  const { mutateAsync: getTopUpQRCode } = useGetTopUpQRCode();
+  const [QRCode, setQRCode] = useState<QRCode>();
+
+  const getQRCode = async () => {
+    await getTopUpQRCode({
+      body: { ref_no: 111111111113, amount: amount },
+      auth: { accessToken, authenticate },
+    })
+      .then((data) => {
+        setQRCode(data);
+      })
+      .catch(() => {});
+  };
+
+  useEffect(() => {
+    getQRCode();
+  }, []);
+
+  const renderImage = useCallback(() => {
+    return (
+      <Image
+        source={{ uri: QRCode?.image ?? MOCKED_QR_CODE_URL }}
+        height={IMAGE_SIZE}
+        width={IMAGE_SIZE}
+        style={styles.image}
+      />
+    );
+  }, [QRCode]);
+
+  const renderEndTimeText = useCallback(() => {
+    const { date, time } = formatDateAndTime(new Date(QRCode?.expire_at ?? ""));
+    return (
+      <BodyText
+        text={`Please pay before ${date} ${time}`}
+        containerStyle={{ alignSelf: "center" }}
+      />
+    );
+  }, [QRCode]);
+
   return (
-    <View>
-      <Text>Scan QR</Text>
-    </View>
+    <BodyContainer innerContainerStyle={styles.innerContainer}>
+      {renderImage()}
+      <View style={styles.textContainer}>
+        <BodyText text="Scan QR Code to pay the bill" textStyle={styles.text} />
+        <BodyText text={`Total amount: ฿ ${amount}`} textStyle={styles.text} />
+      </View>
+      {renderEndTimeText()}
+      <SecondaryButton title="Press to complete payment" onPress={() => {}} />
+    </BodyContainer>
   );
 };
 
 export default PaymentQRCode;
+
+const styles = StyleSheet.create({
+  innerContainer: {
+    gap: 10,
+  },
+  textContainer: {
+    alignItems: "center",
+    paddingVertical: 10,
+    borderRadius: 8,
+    backgroundColor: Colors.red[400],
+  },
+  text: {
+    color: Colors.white,
+  },
+  image: {
+    alignSelf: "center",
+  },
+});
---
+++ store/api/payment/useGetTopUpQRCode/index.ts
@@ -0,0 +1,44 @@
+import {
+  MutationOptions,
+  UseMutationResult,
+  useMutation,
+} from "@tanstack/react-query";
+import { AxiosError } from "axios";
+
+import { PAYMENT_URL } from "..";
+
+import { QRCode } from "@/types/payment";
+import apiRequest, { HTTPMethod } from "@/utils/http";
+
+export type GetTopUpQRCodeInput = {
+  body: {
+    ref_no: number;
+    amount: number;
+  };
+  auth: {
+    accessToken: string;
+    authenticate: (accessToken: string, refreshToken: string) => void;
+  };
+};
+
+type GetTopUpQRCodeService = (input: GetTopUpQRCodeInput) => Promise<QRCode>;
+
+export const getTopUpQRCode: GetTopUpQRCodeService = async ({ body, auth }) => {
+  const data = await apiRequest<QRCode>(
+    PAYMENT_URL + "/payment_v1/payment/topup",
+    HTTPMethod.POST,
+    auth.accessToken,
+    auth.authenticate,
+    body
+  );
+  return data;
+};
+
+export const useGetTopUpQRCode = (
+  options?: MutationOptions<QRCode, AxiosError, GetTopUpQRCodeInput>
+): UseMutationResult<QRCode, AxiosError, GetTopUpQRCodeInput> =>
+  useMutation({
+    mutationFn: getTopUpQRCode,
+    retry: 0,
+    ...options,
+  });
