ce71afd0beea2cca5e0f2245eb39c355c8d066ff
Chayakorn Vongbunsin <taecv.work@gmail.com>
2024-05-03 20:00:27 +0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StatusBar } from "expo-status-bar";
+import { KeyboardAvoidingView } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import BaseNavigation from "./navigation/BaseNavigation";
 import AuthContextProvider from "./store/context/auth";
-import ProfileContextProvider from "./store/context/profile";
 import ParkingLotContextProvider from "./store/context/parkingLot";
+import ProfileContextProvider from "./store/context/profile";
 
 const App = () => {
   const queryClient = new QueryClient();
@@ -14,19 +15,21 @@ const App = () => {
   return (
     <>
       <StatusBar style="dark" />
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <QueryClientProvider client={queryClient}>
-          <AuthContextProvider>
-            <ProfileContextProvider>
-              <ParkingLotContextProvider>
-                <BottomSheetModalProvider>
-                  <BaseNavigation />
-                </BottomSheetModalProvider>
-              </ParkingLotContextProvider>
-            </ProfileContextProvider>
-          </AuthContextProvider>
-        </QueryClientProvider>
-      </GestureHandlerRootView>
+      <KeyboardAvoidingView style={{ flex: 1 }} behavior="height">
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <QueryClientProvider client={queryClient}>
+            <AuthContextProvider>
+              <ProfileContextProvider>
+                <ParkingLotContextProvider>
+                  <BottomSheetModalProvider>
+                    <BaseNavigation />
+                  </BottomSheetModalProvider>
+                </ParkingLotContextProvider>
+              </ProfileContextProvider>
+            </AuthContextProvider>
+          </QueryClientProvider>
+        </GestureHandlerRootView>
+      </KeyboardAvoidingView>
     </>
   );
 };
